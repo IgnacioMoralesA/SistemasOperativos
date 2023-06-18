c0c7fe73f6ee6d2918844466eaf7ae41cb05f9cd
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2023-06-18 15:38:00 -0400

    Update clienteTCP.java

--- src/main/java/clienteTCP.java
+++ src/main/java/clienteTCP.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class clienteTCP {
-
+    //creamos Socket
     static String HOST = "localhost";
     static int PUERTO = 5000;
     Socket sc;
@@ -15,17 +15,21 @@ public class clienteTCP {
         Scanner leer = new Scanner(System.in);
         try{
             sc = new Socket(HOST,PUERTO);
+            //esto inicia la salida de mensajes
             salida = new DataOutputStream(sc.getOutputStream());
+            //aqui se inicia la entrada de mensajes
             entrada = new DataInputStream(sc.getInputStream());
             String mensaje = "";
+            //aqui se realiza el envio de mensaje
             while (!mensaje.equals("fin")){
                 System.out.println("Escriba un mensaje para el servidor");
                 mensaje = leer.nextLine();
                 salida.writeUTF(mensaje);
+                //recepcion de mensajes
                 mensajeRecibido = entrada.readUTF();
                 System.out.println(mensajeRecibido);
             }
-
+            // close de conexion
             sc.close();
         }catch (Exception e){
             e.printStackTrace();
@@ -33,6 +37,7 @@ public class clienteTCP {
     }
 
     public static void main(String[] args) {
+        //main
         clienteTCP ctpc = new clienteTCP();
         ctpc.initCliente();
     }
